108ba31a8e1dd2b025ff371254cf8bbec4e5c446
tatiana <tanianova@yandex.ru>
2021-09-27 18:46:38 +0300

    lesson7-8

--- adminapp/views.py
+++ adminapp/views.py
@@ -6,11 +6,11 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 
 from authapp.models import User
-from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm, ProductAdminCreateForm, ProductAdminUpdateForm
+from mainapp.models import Product
 
 
-
-@user_passes_test(lambda u: u.is_superuser)
+@user_passes_test(lambda u: u.is_superuser, login_url='/')
 def index(request):
     return render(request, 'adminapp/index.html')
 
@@ -21,7 +21,7 @@ class UserListView(ListView):
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(UserListView, self).dispatch(self, request, *args, **kwargs)
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
 class UserCreateView(CreateView):
@@ -32,23 +32,18 @@ class UserCreateView(CreateView):
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(UserCreateView, self).dispatch(self, request, *args, **kwargs)
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
 
 class UserUpdateView(UpdateView):
     model = User
     template_name = 'adminapp/admin-users-update-delete.html'
     form_class = UserAdminProfileForm
-    success_url = reverse_lazy('admins:admin_users_read')
+    success_url = reverse_lazy('adminapp:admin_users_read')
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(UserUpdateView, self).dispatch(self, request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super(UserUpdateView, self).get_context_data(**kwargs)
-        context['title'] = 'geekshop -редактирование пользователя'
-        return context
+        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
 
 
 class UserDeleteView(DeleteView):
@@ -65,6 +60,9 @@ class UserDeleteView(DeleteView):
         self.object.is_active = False
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
+
+
+# FBV:
 # @user_passes_test(lambda u: u.is_superuser)
 # def admin_users_read(request):
 #     context = {
@@ -73,6 +71,7 @@ class UserDeleteView(DeleteView):
 #     return render(request, 'adminapp/admin-users-read.html', context)
 
 
+# @user_passes_test(lambda u: u.is_superuser)
 # def admin_users_create(request):
 #     if request.method == 'POST':
 #         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
@@ -86,8 +85,8 @@ class UserDeleteView(DeleteView):
 #         'form': form,
 #     }
 #     return render(request, 'adminapp/admin-users-create.html', context)
-#
-#
+
+
 # @user_passes_test(lambda u: u.is_superuser)
 # def admin_users_update(request, id=None):
 #     user = User.objects.get(id=id)
@@ -100,11 +99,92 @@ class UserDeleteView(DeleteView):
 #         form = UserAdminProfileForm(instance=user)
 #     context = {'form': form, 'current_user': user}
 #     return render(request, 'adminapp/admin-users-update-delete.html', context)
-#
-#
+
+
 # @user_passes_test(lambda u: u.is_superuser)
 # def admin_users_delete(request, id=None):
 #     user = User.objects.get(id=id)
 #     user.is_active = False
 #     user.save()
 #     return HttpResponseRedirect(reverse('admins:admin_users_read'))
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'adminapp/admin-products-read.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(ProductListView, self).dispatch(request, *args, **kwargs)
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'adminapp/admin-products-create.html'
+    form_class = ProductAdminCreateForm
+    success_url = reverse_lazy('admins:admin_products_read')
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(ProductCreateView, self).dispatch(request, *args, **kwargs)
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'adminapp/admin-products-update-delete.html'
+    form_class = ProductAdminUpdateForm
+    success_url = reverse_lazy('adminapp:admin_products_read')
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(ProductUpdateView, self).dispatch(request, *args, **kwargs)
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'adminapp/admin-users-update-delete.html'
+    success_url = reverse_lazy('adminapp:admin_products_read')
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(ProductDeleteView, self).dispatch(request, *args, **kwargs)
+
+
+
+# FBV:
+# def admin_products_read(request):
+#     context = {'products': product.objects.all()}
+#     return render(request, 'adminapp/admin-products-read.html', context)
+
+
+# def admin_products_create(request):
+#     if request.method == 'POST':
+#         form = ProductAdminCreateForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_products_read'))
+#     else:
+#         form = ProductAdminCreateForm()
+#     context = {'form': form}
+#     return render(request, 'adminapp/admin-products-create.html', context)
+
+
+# def admin_products_update(request, id=None):
+#     current_product = product.objects.get(id=id)
+#     if request.method == 'POST':
+#         form = ProductAdminUpdateForm(data=request.POST, files=request.FILES, instance=current_product)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('adminapp:admin_products_read'))
+#     else:
+#         form = ProductAdminUpdateForm(instance=current_product)
+#     context = {
+#         'form': form,
+#         'current_product': current_product
+#     }
+#     return render(request, 'adminapp/admin-products-update-delete.html', context)
+
+
+# def admin_products_delete(request, id=None):
+#     current_product = product.objects.get(id=id)
+#     current_product.delete()
+#     return HttpResponseRedirect(reverse('admins:admin_products_read'))
--- authapp/models.py
+++ authapp/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatar',blank=True)
-    age=models.PositiveSmallIntegerField(blank=True,null=True)
+    # age=models.PositiveSmallIntegerField(blank=True,null=True)
+    age=models.PositiveSmallIntegerField(verbose_name='возраст',default=18)
+    language = models.CharField(max_length=3,default='EN')
