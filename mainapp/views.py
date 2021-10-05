e2e453369ab5cb6690b77c43631f2670685a1026
tatiana <tanianova@yandex.ru>
2021-10-05 20:54:41 +0300

    added product_detail

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
 from mainapp.models import Product, ProductCategory
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -26,3 +27,13 @@ def products(request, category_id=None, page=1):
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'mainapp/products.html', context)
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'mainapp/product_detail.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, category_id=None, *args, **kwargs):
+        context = super().get_context_data()
+        context['categories'] = ProductCategory.objects.all()
+        return context
